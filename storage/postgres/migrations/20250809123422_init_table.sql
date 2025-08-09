5c1906c539d979904223142e57d9837e8fd25140
Selyushkin Dmitry <teryble09@yandex.ru>
2025-08-09 15:45:53 +0300

    adds goose migration

---
+++ storage/postgres/migrations/20250809123422_init_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE subscriptions (
+    id SERIAL PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+CREATE INDEX idx_subscriptions_start_date ON subscriptions(start_date);
+CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE subscriptions;
+-- +goose StatementEnd
