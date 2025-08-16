84045bcaf775ec357ca67c0de24fefdadb4e6ff9
Selyushkin Dmitry <teryble09@yandex.ru>
2025-08-16 12:29:53 +0300

    [refactor] redundant code

--- lib/dateparse/parse.go
+++ lib/dateparse/parse.go
@@ -5,10 +5,6 @@ import "time"
 const layout = "01-2006"
 
 func ParseMMYYYY(date string) (time.Time, error) {
-	// in case if user wants to set end date to null
-	if date == "" {
-		return time.Time{}, nil
-	}
 	return time.Parse(layout, date)
 }
 
